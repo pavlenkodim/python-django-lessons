206ddc386fecf0fa6f3c196d29d59e3e0cd78067
Dmitry Pavlenko <pavlenkodim@mail.ru>
2026-04-29 21:05:03 +0500

    Lesson 4 - update (add kind to forms)

--- djangotutorial/bboard/forms.py
+++ djangotutorial/bboard/forms.py
@@ -4,4 +4,4 @@ from .models import Bd
 class BbFrom(ModelForm):
     class Meta:
         model = Bd
-        fields = ('title', 'content', 'price', 'rubric')
+        fields = ('title', 'content', 'price', 'rubric', 'kind')
